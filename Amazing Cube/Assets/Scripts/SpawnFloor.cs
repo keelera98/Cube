ff497feb1d80d3dc6d7961972fa648c1c714cfc9
keelera98 <32946655+keelera98@users.noreply.github.com>
2019-10-09 19:46:20 -0400

    Initial Files

---
+++ Amazing Cube/Assets/Scripts/SpawnFloor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFloor : MonoBehaviour
+{
+    public GameObject floorTile;
+    public int floorSize;
+    public GameObject[,] Floor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Floor = new GameObject[floorSize, floorSize];
+
+        for (int x = 0; x < floorSize; x++)
+        {
+            for (int z = 0; z < floorSize; z++)
+            {
+                Floor[x,z] = Instantiate(floorTile, new Vector3(x, 0, z), Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
