d3f246eebd812fd5ec50f28d71f048cf752f7f8c
keelera98 <32946655+keelera98@users.noreply.github.com>
2019-11-13 19:20:18 -0500

    Remove Amazing Cube folder

---
+++ Assets/Scripts/TileInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileInfo : MonoBehaviour
+{
+    public bool isEnd;
+
+    void Start()
+    {
+        if(isEnd)
+        {
+            var tileRenderer = gameObject.GetComponent<Renderer>();
+
+            //Call SetColor using the shader property name "_Color" and setting the color to red
+            tileRenderer.material.SetColor("_Color", Color.green);
+        }
+    }
+}
