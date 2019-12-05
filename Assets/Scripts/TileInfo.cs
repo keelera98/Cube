ff3fc620edc75c58cb3b43ddaf1f80e9c05a662a
keelera98 <32946655+keelera98@users.noreply.github.com>
2019-12-04 19:44:27 -0500

    Update Path

--- Assets/Scripts/TileInfo.cs
+++ Assets/Scripts/TileInfo.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TileInfo : MonoBehaviour
 {
     public bool isEnd;
+    private bool isColored = false;
 
     void Start()
     {
@@ -12,8 +13,25 @@ public class TileInfo : MonoBehaviour
         {
             var tileRenderer = gameObject.GetComponent<Renderer>();
 
-            //Call SetColor using the shader property name "_Color" and setting the color to red
+            //Call SetColor using the shader property name "_Color" and setting the color to green
             tileRenderer.material.SetColor("_Color", Color.green);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        isColored = true;
+    }
+
+    void Update()
+    {
+        if (isColored)
+        {
+            var tileRenderer = gameObject.GetComponent<Renderer>();
+
+            GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+
+            isColored = false;
         }
     }
 }
