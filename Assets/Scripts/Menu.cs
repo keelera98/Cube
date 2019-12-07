d61302dbe05423e22cfebab00e30923150c31ecc
Rossi <cjr07307@b101-015.academia.sjfc.edu>
2019-12-07 17:16:15 -0500

    added menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void StartButton()
+    {
+        SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
+    }
+    public void MenuButton()
+    {
+        SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+    }
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
